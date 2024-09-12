3421664b7198fe87415d305a768c284d3e57c9c1
QuantumShinobi <63300310+QuantumShinobi@users.noreply.github.com>
2024-09-12 11:43:05 +0530

    Random commits

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -50,8 +50,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["NextJS", "Django", "Python"];
+  const rightLists = ["Flutter", "TypeScript", "GraphQL"];
 
   const [copied, setCopied] = useState(false);
 
@@ -65,7 +65,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "mail.rishit.verma@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
